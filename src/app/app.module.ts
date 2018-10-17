ed02e5e54d5c303f33552b9cefabb87a47cc7147
Erick Gonzalez <erick.dg.aguilar@gmail.com>
2018-10-17 16:30:46 -0500

    Modificaciones en Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,28 +10,10 @@ import { APP_ROUTING } from './app.routes';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
-import { HeaderComponent } from './components/shared/header/header.component';
-import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './components/shared/breadcrumbs/breadcrumbs.component';
-import { NoPageFoundComponent } from './components/shared/no-page-found/no-page-found.component';
-
-import { PagesComponent } from './components/pages/pages.component';
-import { ProgressComponent } from './components/pages/progress/progress.component';
-import { Graficas1Component } from './components/pages/graficas1/graficas1.component';
-import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    // HeaderComponent,
-    // SidebarComponent,
-    // BreadcrumbsComponent,
-    // NoPageFoundComponent,
-    // DashboardComponent,
-    // ProgressComponent
-    // PagesComponent,
-    // Graficas1Component,
+    LoginComponent
   ],
   imports: [
   BrowserModule,
